2cbc1509ef13a830ad7a3809d82a30542c2cb321
Rohan Joshi <rohanjoshirohanjoshi189@gmail.com>
2026-06-11 18:27:13 +0530

    feat: implement database connection and update server configuration

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -6,7 +13,7 @@ app.get("/", (req, res) => {
   res.send("Task Tracker API Running...");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
