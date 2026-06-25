0af3a4339c1b625290c183f378063a7459f56182
Rohan Joshi <rohanjoshirohanjoshi189@gmail.com>
2026-06-25 16:12:35 +0530

    feat: add task comments schema

--- models/Task.js
+++ models/Task.js
@@ -40,6 +40,25 @@ const taskSchema = new mongoose.Schema(
       ref: "User",
       required: true,
     },
+
+    comments: [
+      {
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+
+        text: {
+          type: String,
+          required: true,
+        },
+
+        createdAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
   },
   {
     timestamps: true,
