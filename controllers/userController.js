b227a23bbbdcbe171ceaacb084e67816c60ce853
Rohan Joshi <rohanjoshirohanjoshi189@gmail.com>
2026-06-25 15:29:02 +0530

    feat: add admin update user  & change-password API

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
 const getUsers = async (req, res) => {
   try {
@@ -79,6 +80,35 @@ const updateUser = async (req, res) => {
   }
 };
 
+const changePassword = async (req, res) => {
+  try {
+    const { oldPassword, newPassword } = req.body;
+
+    const user = await User.findById(req.user._id);
+
+    const isMatch = await bcrypt.compare(oldPassword, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Old password is incorrect",
+      });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(newPassword, salt);
+
+    await user.save();
+
+    res.status(200).json({
+      message: "Password changed successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
 const deleteUser = async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
@@ -105,5 +135,6 @@ module.exports = {
   getUsers,
   deleteUser,
   updateProfile,
-  updateUser
+  updateUser,
+  changePassword
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { getUsers, deleteUser, updateProfile, updateUser } = require("../controllers/userController");
+const { getUsers, deleteUser, updateProfile, updateUser, changePassword } = require("../controllers/userController");
 
 const { protect } = require("../middleware/authMiddleware");
 const { authorize } = require("../middleware/roleMiddleware");
 
 router.get("/", protect, authorize("admin", "manager"), getUsers);
 router.patch("/profile", protect, updateProfile);
+router.patch("/change-password", protect, changePassword);
 router.patch("/:id", protect, authorize("admin"), updateUser);
 router.delete("/:id", protect, authorize("admin"), deleteUser);
 
