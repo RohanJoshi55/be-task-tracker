40a74fea373c58ef746b5954035e7aeac6d5ce10
Rohan Joshi <rohanjoshirohanjoshi189@gmail.com>
2026-06-25 13:07:05 +0530

    feat: add delete user API

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,6 +12,29 @@ const getUsers = async (req, res) => {
   }
 };
 
+const deleteUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    await User.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({
+      message: "User deleted successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
 module.exports = {
   getUsers,
+  deleteUser,
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { getUsers } = require("../controllers/userController");
+const { getUsers, deleteUser } = require("../controllers/userController");
 
 const { protect } = require("../middleware/authMiddleware");
 const { authorize } = require("../middleware/roleMiddleware");
 
 router.get("/", protect, authorize("admin", "manager"), getUsers);
+router.delete("/:id", protect, authorize("admin"), deleteUser);
 
 module.exports = router;
